8d0309c33f6748072a57ce5a74d645df91014f78
Johan Liu <liujiahan@gmail.com>
2016-08-15 17:28:21 +0800

    Add frame size option to udpsend.

--- src/udpsend.c
+++ src/udpsend.c
@@ -4,7 +4,10 @@
 #include <sys/time.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <time.h>
+#include <string.h>
 
 #ifdef __VXWORKS__
 #include <sockLib.h>
@@ -12,6 +15,7 @@
 
 static struct sockaddr_in sa;
 static int sa_len;
+static char *buffer;
 
 void usage()
 {
@@ -42,42 +46,54 @@ double getTime()
 
 #endif /* USE_GETTIMEOFDAY */
 
-void SendMsg(int fd, int i, double t)
-{
-	size_t n;
-	static char buffer[128];
-	n=sprintf(buffer,"%d:%lf\n",i,t);
-	sendto(fd, buffer, n, 0, (struct sockaddr*)&sa, sa_len);
-	//puts(buffer);
-}
-
 int main(int argc, char **argv)
 {
+	int next_opt;
 	int fd;
 	short dst_port=9999;
 	char *dst_ip="192.168.0.255";
+	unsigned short port=0;
 	unsigned long idx=1;
 	double last, now, span;
+	size_t size=1024;
+	char *ipstr=NULL;
+	double interval=0.001;
 
-	if(argc > 3)
+	do{
+		next_opt=getopt(argc,argv,"b:a:p:i:h");
+		switch(next_opt){
+			case 'b': size=atol(optarg); break;
+			case 'a': ipstr=optarg; break;
+			case 'p': port=atoi(optarg); break;
+			case 'i': interval=atof(optarg); break;
+			case 'h': usage(); break;
+			case -1: break;
+			default: fprintf(stderr, "Unknown option -%c %s\n",
+						 next_opt, optarg);
+				 fprintf(stderr, "Use -h for help\n");
+				 return -1;
+		}
+	}while(next_opt != -1);
+
+	if(argc-optind > 2)
 	{
 		fprintf(stderr,"Incorrect arguments\n");
 		usage();
 		return -1;
 	}
-	else if(argc > 2)
+	else if(argc-optind > 1)
 	{
-		if(sscanf(argv[2],"%d",&dst_port)!=1)
+		if(sscanf(argv[optind+1],"%d",&dst_port)!=1)
 		{
 			fprintf(stderr,"Incorrect port number\n");
 			usage();
 			return -1;
 		}
-		dst_ip=argv[1];
+		dst_ip=argv[optind];
 	}
-	else if(argc > 1)
+	else if(argc-optind > 0)
 	{
-		dst_ip=argv[1];
+		dst_ip=argv[optind];
 	}
 
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -85,7 +101,16 @@ int main(int argc, char **argv)
 		return -1;
 	sa.sin_family = AF_INET;
 	sa.sin_addr.s_addr = htonl(INADDR_ANY);
-	sa.sin_port = htons(0);
+	sa.sin_port = htons(port);
+	if(ipstr){
+		struct hostent *he;
+		he=gethostbyaddr(ipstr,strlen(ipstr),AF_INET);
+		if(he==NULL){
+			herror("Invalid address\n");
+			return -1;
+		}
+		memcpy(&(sa.sin_addr.s_addr),he->h_addr_list[0],sizeof(in_addr_t));
+	}
 	sa_len = sizeof(sa);
 
 	if(bind(fd, (struct sockaddr *)&sa, sa_len) < 0)
@@ -94,14 +119,20 @@ int main(int argc, char **argv)
 	sa.sin_addr.s_addr = inet_addr(dst_ip);
 	sa.sin_port = htons(dst_port);
 
+	if(size < 32)
+		size = 32;
+
+	buffer = malloc(size);
+
 	last=getTime();
 	while(1)
 	{
 		now=getTime();
 		span=now-last;
-		if(span>0.001)
+		if(span > interval)
 		{
-			SendMsg(fd, idx++,now);
+			sprintf(buffer,"%08d:%022.6lf\n",idx++,now);
+			sendto(fd, buffer, size, 0, (struct sockaddr*)&sa, sa_len);
 			last=now;
 		}
 	}
